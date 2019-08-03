4eb849dfb2bf2a2e74a1247c561c6fab24db4af7
Joshua Hill <josh@jlhworkshop.com>
2019-08-03 03:21:00 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,5 +1,11 @@
 def run_guessing_game
-  x = rand(1..6)
+# why doesn't this work? i can't see any reason in the documentation that
+# this shouldn't do the exact same thing the code in the solution
+# accomplishes. i know i'm a ruby novice, but i feel like i've noticed
+# this phenomenon several times in these labs of things that by external
+# accounts should work but don't
+#  x = rand(1..6)
+x = rand
   puts "Guess a number between 1-6:"
   input = gets.chomp
   if input == x.to_s
